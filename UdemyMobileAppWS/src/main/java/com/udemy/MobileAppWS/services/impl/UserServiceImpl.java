2cc6c43c3c8b344da7803005a70ad8463f7d8605
therealfigge <fredrik.strandberg@outlook.com>
2023-09-16 16:52:56 +0200

    Adding ServiceLayer
    Adding UserService and UserServiceImpl to start a service layer.

---
+++ UdemyMobileAppWS/src/main/java/com/udemy/MobileAppWS/services/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.udemy.MobileAppWS.services.impl;
+
+import com.udemy.MobileAppWS.dto.UserDto;
+import com.udemy.MobileAppWS.services.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author fredr
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public UserDto createUser(UserDto user) {
+        ;
+    }
+    
+}
--- UdemyMobileAppWS/src/main/java/com/udemy/MobileAppWS/ui/controller/UserController.java
+++ UdemyMobileAppWS/src/main/java/com/udemy/MobileAppWS/ui/controller/UserController.java
@@ -4,8 +4,12 @@
  */
 package com.udemy.MobileAppWS.ui.controller;
 
+import com.udemy.MobileAppWS.dto.UserDto;
 import com.udemy.MobileAppWS.model.request.UserDetailsRequestModel;
 import com.udemy.MobileAppWS.model.response.UserResponse;
+import com.udemy.MobileAppWS.services.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,9 +27,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("users")
 public class UserController {
+    
+    @Autowired
+    UserService userService;
+    
     @PostMapping
     public ResponseEntity<UserResponse> createUser(@RequestBody UserDetailsRequestModel userDetails) {
         UserResponse userResponse = new UserResponse();
+        
+        UserDto userDto = new UserDto();
+        BeanUtils.copyProperties(userDetails, userDto);
+        
+        UserDto createdUser = userService.createUser(userDto);
+        
+        BeanUtils.copyProperties(createdUser, userResponse);
         
         return new ResponseEntity(userResponse, HttpStatus.CREATED);
     }
