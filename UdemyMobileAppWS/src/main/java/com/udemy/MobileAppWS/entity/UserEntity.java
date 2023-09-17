ebb15e87f2e262b7757a9aa9a96c6a983c9e9630
therealfigge <fredrik.strandberg@outlook.com>
2023-09-17 23:32:34 +0200

    Adding class UserEntity
    Creating a user entity class for persisiting data.

---
+++ UdemyMobileAppWS/src/main/java/com/udemy/MobileAppWS/entity/UserEntity.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.udemy.MobileAppWS.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+/**
+ *
+ * @author fredr
+ */
+@Entity(name="users")
+public class UserEntity implements Serializable {
+    private static final long serialVersionUID = 6553532122368L;
+    
+    @Id
+    @GeneratedValue
+    private long id;
+        
+    @Column(nullable=false)
+    private String userId;
+    
+    @Column(nullable=false, length=50)
+    private String firstName;
+    
+    @Column(nullable=false, length=50)
+    private String lastName;
+    
+    @Column(nullable=false, length=100)
+    private String email;
+    
+    @Column(nullable=false)
+    private String encryptedPassword;
+    
+    private String emailVerificationToken;
+
+    @Column(nullable=false, columnDefinition = "boolean default false")
+    private Boolean emailVerificationStatus;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public String getEmailVerificationToken() {
+        return emailVerificationToken;
+    }
+
+    public void setEmailVerificationToken(String emailVerificationToken) {
+        this.emailVerificationToken = emailVerificationToken;
+    }
+
+    public Boolean getEmailVerificationStatus() {
+        return emailVerificationStatus;
+    }
+
+    public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
+        this.emailVerificationStatus = emailVerificationStatus;
+    }
+    
+}
