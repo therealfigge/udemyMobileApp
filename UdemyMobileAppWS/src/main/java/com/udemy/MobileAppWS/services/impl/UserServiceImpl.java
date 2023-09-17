28c1fb959ed79f9330bc746011b33fefacb01d66
therealfigge <fredrik.strandberg@outlook.com>
2023-09-18 00:11:25 +0200

    Updated UserServiceImpl
    Updated UserServiceImpl with autowiring UserRepository to be able to persist data in MySQL. Tested the code E2E with a successful storage of a user.

--- UdemyMobileAppWS/src/main/java/com/udemy/MobileAppWS/services/impl/UserServiceImpl.java
+++ UdemyMobileAppWS/src/main/java/com/udemy/MobileAppWS/services/impl/UserServiceImpl.java
@@ -5,7 +5,11 @@
 package com.udemy.MobileAppWS.services.impl;
 
 import com.udemy.MobileAppWS.dto.UserDto;
+import com.udemy.MobileAppWS.entity.UserEntity;
+import com.udemy.MobileAppWS.repositories.UserRepository;
 import com.udemy.MobileAppWS.services.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,10 +18,25 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserServiceImpl implements UserService {
+    
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public UserDto createUser(UserDto user) {
-        return null;
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+        
+        // TODO - Remove the following two lines
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId("testUserId");
+        
+        UserEntity storedValue = userRepository.save(userEntity);
+        
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(storedValue, returnValue);
+        
+        return returnValue;
     }
     
 }
