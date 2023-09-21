f9cc5749ea4316fe104dfba41efd8412f41f978f
therealfigge <fredrik.strandberg@outlook.com>
2023-09-21 23:31:47 +0200

    UserEntity
    UserEntity class has been created and developed.

--- MobileAppWS/src/main/java/com/udemy/udemyTrainingAppWS/io/entity/UserEntity.java
+++ MobileAppWS/src/main/java/com/udemy/udemyTrainingAppWS/io/entity/UserEntity.java
@@ -2,24 +2,45 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.udemy.udemyTrainingAppWS.shared.dto;
+package com.udemy.udemyTrainingAppWS.io.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import java.io.Serializable;
 
 /**
  *
  * @author fredr
  */
-public class UserDto implements Serializable {
-    private static final long serialVersionUID = 23458973456L;
+@Entity(name="users")
+public class UserEntity implements Serializable {
+    
+    private static final long serialVersionUID = 2007L;
+    
+    @Id
+    @GeneratedValue
     private long id;
+    
+    @Column(nullable=false)
     private String userId;
+    
+    @Column(nullable=false, length=50)
     private String firstName;
+    
+    @Column(nullable=false, length=50)
     private String lastName;
+    
+    @Column(nullable=false, length=120)
     private String email;
-    private String password;
+    
+    @Column(nullable=false)
     private String encryptedPassword;
+    
     private String emailVerificationToken;
+    
+    @Column(nullable=false)
     private Boolean emailVerificationStatus = false;
 
     public long getId() {
@@ -60,14 +81,6 @@ public class UserDto implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getEncryptedPassword() {
