494816f58bec1a78111301c35ab34febfd8f8688
therealfigge <fredrik.strandberg@outlook.com>
2023-09-21 23:51:39 +0200

    Update UserServiceImpl.java
    Entered actions to persist data into database.

--- MobileAppWS/src/main/java/com/udemy/udemyTrainingAppWS/service/impl/UserServiceImpl.java
+++ MobileAppWS/src/main/java/com/udemy/udemyTrainingAppWS/service/impl/UserServiceImpl.java
@@ -4,8 +4,12 @@
  */
 package com.udemy.udemyTrainingAppWS.service.impl;
 
+import com.udemy.udemyTrainingAppWS.io.entity.UserEntity;
+import com.udemy.udemyTrainingAppWS.repository.UserRepository;
 import com.udemy.udemyTrainingAppWS.service.UserService;
 import com.udemy.udemyTrainingAppWS.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,10 +18,25 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserServiceImpl implements UserService {
+    
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public UserDto createUser(UserDto user) {
-        return null;
+        
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+        
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId("testUserId");
+        
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+        
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(storedUserDetails, returnValue);
+        
+        return returnValue;
     }
     
 }
