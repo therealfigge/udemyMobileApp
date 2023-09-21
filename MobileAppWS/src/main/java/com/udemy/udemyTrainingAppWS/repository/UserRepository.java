1f12e7842d9f5157d3dd255133706d2b70319723
therealfigge <fredrik.strandberg@outlook.com>
2023-09-21 23:40:30 +0200

    Create UserRepository.java
    Added UserRepository class

---
+++ MobileAppWS/src/main/java/com/udemy/udemyTrainingAppWS/repository/UserRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.udemy.udemyTrainingAppWS.repository;
+
+import com.udemy.udemyTrainingAppWS.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author fredr
+ */
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    
+}
